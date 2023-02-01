574b84dd6f70e0c2f2bf2ab547e381f6b138115b
Dandandan-mo <dan.mo6304@gmail.com>
2023-02-01 18:38:56 +1100

    updated OrderHistory Componenet

--- src/components/OrderHistory.jsx
+++ src/components/OrderHistory.jsx
@@ -4,6 +4,7 @@ const OrderHistory = () => {
   return (
     <>
     <h3>Order History</h3>
+    <p>This is an optional feature of the application according to the design idea. Due to time limit this feature is not yet developed.</p>
   </>
   )
 }
